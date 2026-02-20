acbecad19de05324547779db6b3027b05788b7c4
nasgmr <noviaazkasalsabila@gmail.com>
2026-02-20 14:04:57 +0700

    update: adding productRouter

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Konfigurasi EJS sebagai mesin template
+const indexRouter = require('./routes/index');
+const productRouter = require('./routes/productRoutes');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Mengatur folder 'public' sebagai tempat aset statis (CSS, Gambar)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rute (Route) untuk Halaman Utama
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/', indexRouter);
+app.use('/', productRouter);
 
-// Rute sementara agar saat menu lain diklik tidak muncul pesan error (404 Not Found)
 app.get('/*', (req, res) => {
-    res.send('<h1 style="text-align:center; margin-top:50px;">Halaman sedang dalam tahap pengembangan.</h1><p style="text-align:center;"><a href="/">Kembali ke Home</a></p>');
+    res.send('<h1 style="text-align:center; margin-top:50px;">This page is still being developed.</h1><p style="text-align:center;"><a href="/">Back to Home</a></p>');
 });
 
-// Menyalakan server pada Port 3000
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server AMX UAV berhasil berjalan di http://localhost:${PORT}`);
+    console.log(`Server AMX UAV berjalan di http://localhost:${PORT}`);
 });
