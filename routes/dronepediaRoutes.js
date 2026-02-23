7f1b8b4e15bf84628fbfafc17b817cebb9bcea8e
nasgmr <noviaazkasalsabila@gmail.com>
2026-02-23 16:29:18 +0700

    update: dronepedia done

--- routes/dronepediaRoutes.js
+++ routes/dronepediaRoutes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/dronepedia', (req, res) => {
-    res.render('dronepedia'); 
+// Redirect otomatis ke tab pertama
+router.get('/resources/dronepedia', (req, res) => {
+    res.redirect('/resources/dronepedia/how-it-works');
+});
+
+// Tab 1: How it Works
+router.get('/resources/dronepedia/how-it-works', (req, res) => {
+    res.render('dronepedia-how-it-works'); 
+});
+
+// Tab 2 & 3 (Siapkan rutenya dulu untuk nanti)
+router.get('/resources/dronepedia/classifications', (req, res) => {
+    res.render('dronepedia-classifications'); 
+});
+
+router.get('/resources/dronepedia/applications', (req, res) => {
+    res.render('dronepedia-applications'); 
 });
 
 module.exports = router;
