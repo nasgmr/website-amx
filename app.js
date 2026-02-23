2108e1fdd945efba37f09d30304776678417ed3e
nasgmr <noviaazkasalsabila@gmail.com>
2026-02-23 13:46:47 +0700

    update: fixing resources-main

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const indexRouter = require('./routes/index');
 const productRouter = require('./routes/productRoutes');
 const serviceRouter = require('./routes/serviceRoutes');
+const resourcesRoutes = require('./routes/resourcesRoutes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', productRouter);
 app.use('/', serviceRouter);
+app.use('/', resourcesRoutes);
 
 app.get('/*', (req, res) => {
     res.send('<h1 style="text-align:center; margin-top:50px;">This page is still being developed.</h1><p style="text-align:center;"><a href="/">Back to Home</a></p>');
