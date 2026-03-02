e3b249cd5a830c15f913a3999825c84c2b12a5d0
nasgmr <noviaazkasalsabila@gmail.com>
2026-03-02 16:33:57 +0700

    update: organized more neatly

--- routes/dronepediaRoutes.js
+++ routes/dronepediaRoutes.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Redirect otomatis ke tab pertama
 router.get('/products/dronepedia', (req, res) => {
     res.redirect('/products/dronepedia/how-it-works');
 });
 
-// Tab 1: How it Works
 router.get('/products/dronepedia/how-it-works', (req, res) => {
     res.render('dronepedia-how-it-works'); 
 });
 
-// Tab 2 & 3 (Siapkan rutenya dulu untuk nanti)
 router.get('/products/dronepedia/classifications', (req, res) => {
     res.render('dronepedia-classifications'); 
 });
